bfa549cd2e354d049f8842c3f0540db3adada59c
JB Bell <tquidca@gmail.com>
2023-02-04 12:11:54 -0800

    a30

--- activities/src/bin/a30.rs
+++ activities/src/bin/a30.rs
@@ -18,4 +18,39 @@
 trait Body {}
 trait Color {}
 
-fn main() {}
+struct Vehicle<T: Body, U: Color> {
+    body: T,
+    color: U,
+}
+
+impl<T: Body, U: Color> Vehicle<T, U> {
+    fn new(body: T, color: U) -> Self {
+        Self {
+            body,
+            color,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Truck;
+impl Body for Truck {}
+
+#[derive(Debug)]
+struct Bicycle;
+impl Body for Bicycle {}
+
+#[derive(Debug)]
+struct Red;
+impl Color for Red {}
+
+#[derive(Debug)]
+struct Blue;
+impl Color for Blue {}
+
+fn main() {
+    let veras_bike = Vehicle::new(Bicycle, Red);
+    let bobs_truck = Vehicle::new(Truck, Blue);
+    println!("veras_bike is a {:?}-colored {:?}", veras_bike.color, veras_bike.body);
+    println!("bobs_truck is a {:?}-colored {:?}", bobs_truck.color, bobs_truck.body);
+}
