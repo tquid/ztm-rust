2e3e7c69e27c956c227bd70d9321e6a302341ec7
JB Bell <tquidca@gmail.com>
2023-02-20 09:21:16 -0800

    After form field setup

--- clipstash/src/lib/domain/clip/field/expires.rs
+++ clipstash/src/lib/domain/clip/field/expires.rs
@@ -1,5 +1,6 @@
-use crate::domain::time::Time;
 use crate::domain::clip::ClipError;
+use crate::domain::time::Time;
+use rocket::form::{self, FromFormField, ValueField};
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 
@@ -13,7 +14,6 @@ impl Expires {
     pub fn into_inner(self) -> Option<Time> {
         self.0
     }
-
 }
 
 impl Default for Expires {
@@ -30,8 +30,19 @@ impl FromStr for Expires {
         } else {
             match Time::from_str(raw) {
                 Ok(time) => Ok(Self::new(time)),
-                Err(e) => Err(e.into())
+                Err(e) => Err(e.into()),
             }
+        }
+    }
+}
+
+#[rocket::async_trait]
+impl<'r> FromFormField<'r> for Expires {
+    fn from_value(field: ValueField<'r>) -> form::Result<'r, Self> {
+        if field.value.trim().is_empty() {
+            Ok(Self(None))
+        } else {
+            Ok(Self::from_str(field.value).map_err(|e| form::Error::validation(format!("{}", e)))?)
         }
     }
 }
