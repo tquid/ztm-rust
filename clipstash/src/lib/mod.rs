884b16278a33cd130af9b3f803c5c68979e9f4ee
JB Bell <tquidca@gmail.com>
2023-02-23 17:02:38 -0800

    Hit counter pt 1

--- clipstash/src/lib/mod.rs
+++ clipstash/src/lib/mod.rs
@@ -12,12 +12,14 @@ pub use service::ServiceError;
 use data::AppDatabase;
 use rocket::fs::FileServer;
 use rocket::{Build, Rocket};
+use web::hitcounter::HitCounter;
 use web::{renderer::Renderer};
 
 pub fn rocket(config: RocketConfig) -> Rocket<Build> {
     rocket::build()
         .manage::<AppDatabase>(config.database)
         .manage::<Renderer>(config.renderer)
+        .manage::<HitCounter>(config.hit_counter)
         .mount("/", web::http::routes())
         .mount("/static", FileServer::from("static"))
         .register("/", web::http::catcher::catchers())
@@ -26,4 +28,5 @@ pub fn rocket(config: RocketConfig) -> Rocket<Build> {
 pub struct RocketConfig {
     pub renderer: Renderer<'static>,
     pub database: AppDatabase,
+    pub hit_counter: HitCounter,
 }
